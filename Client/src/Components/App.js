250408011564da437a5f821324a2b9aa66bb7069
esle <essi.lehtola@hotmail.com>
2019-01-16 15:55:54 +0200

    react + api ready to use

---
+++ Client/src/Components/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+
+class App extends Component {
+    state = { walletInfo: {} }
+
+    componentDidMount() {
+        Axios.get('/api/wallet-info')
+            .then(response => {
+                this.setState({ walletInfo: response.data });
+                //console.log(this.state);                
+            });
+    }
+
+    render() {
+        const { address, balance } = this.state.walletInfo;
+
+        this.componentDidMount
+        return (
+            <div>
+                <div>App component</div>
+                <div>Address: {address}</div>
+                <div>Balance: {balance}</div>
+            </div>
+        );
+    }
+}
+
+export default App;
--- Client/src/index.js
+++ Client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './Components/App';
 
 render(
-    <div>Testi</div>,
+    <App />,
     document.getElementById('root')
 );
